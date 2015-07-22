8352ca1d167e7673b126c4258cb6cbc98824bbf0
yussan <yusuf.hi@students.amikom.ac.id>
2015-07-22 22:48:37 +0700

    header almost

--- assets/tecs.js
+++ assets/tecs.js
@@ -1,1 +1,9 @@
 var app = angular.module('TecsApp',[]);//generate app
+//HEADER CONTROLLER
+app.controller('CtrlHeader',['$scope',function($scope){
+	console.log('CtrlHeader is ready');
+}]);
+//TABULAR CONTROLLER
+app.controller('CtrlTabular', ['$scope', function($scope){
+	console.log('CtrlTabular is ready');
+}]);
