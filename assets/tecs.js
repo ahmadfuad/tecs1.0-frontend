c11e017735c9e82c11fe38e0ea82c4ae5bc36954
yussan <yusuf.hi@students.amikom.ac.id>
2015-07-23 17:30:55 +0700

    finishing

--- assets/tecs.js
+++ assets/tecs.js
@@ -7,3 +7,7 @@ app.controller('CtrlHeader',['$scope',function($scope){
 app.controller('CtrlTabular', ['$scope', function($scope){
 	console.log('CtrlTabular is ready');
 }]);
+//BARANG CONTROLLER
+app.controller('CtrlBarang', ['$scope', function($scope){
+	$scope.items = [1,2,3,4,5,6,7,8,9,10,11,12];
+}])
